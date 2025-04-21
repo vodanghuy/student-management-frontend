64e26d185f454e6ecfc716a3bf563496c066d9c0
Vo Dang Huy <iamdaghuy@gmail.com>
2025-04-21 15:16:53 +0700

    add, edit, delete,search subject

--- src/app/subject/subject.component.ts
+++ src/app/subject/subject.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { SubjectService } from '../subject.service';
 import { Subject } from '../subject';
 import { HttpErrorResponse } from '@angular/common/http';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-subject',
@@ -12,6 +13,9 @@ import { HttpErrorResponse } from '@angular/common/http';
 export class SubjectComponent implements OnInit{
 
   public subjects: Subject[] = [];
+  public updateSubject: Subject | undefined;
+  public deleteSubject: Subject | undefined;
+
   ngOnInit(): void {
       this.getSubjects();
   }
@@ -28,7 +32,61 @@ export class SubjectComponent implements OnInit{
     )
   }
 
-  public onOpenModal(subject: Subject, mode: string): void{
+  public addSubject(addForm: NgForm): void{
+    this.subjectService.createSubject(addForm.value).subscribe(
+      (response: Subject) => {
+        console.log(response);
+        document.getElementById('add-subject-form')?.click();
+        this.getSubjects();
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    )
+  }
+
+  public onUpdateSubject(subject: Subject): void{
+    this.subjectService.updateSubject(subject).subscribe(
+      (response: Subject) => {
+        console.log(response);
+        this.getSubjects();
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    )
+  }
+
+  public onDeleteSubject(): void{
+    if(this.deleteSubject){
+      this.subjectService.deleteSubject(this.deleteSubject.id).subscribe(
+        (response: void) => {
+          console.log(response);
+          this.getSubjects();
+        },
+        (error: HttpErrorResponse) => {
+          alert(error.message);
+        }
+      )
+    }
+  }
+
+  public searchSubject(key: string): void{
+    key = key ? key.trim().toLowerCase() : '';
+    const result: Subject[] = [];
+    for (const subject of this.subjects) {
+      if(subject.id.trim().toLowerCase().indexOf(key) !== -1 ||
+      subject.name.trim().toLowerCase().indexOf(key) !== -1){
+        result.push(subject);
+      }
+    }
+    this.subjects = result;
+    if(result.length === 0 || !key){
+      this.getSubjects();
+    }
+  }
+
+  public onOpenModal(subject: Subject | null, mode: string): void{
     const container = document.getElementById('main-container');
     const button = document.createElement('button');
     button.style.display = 'none';
@@ -38,9 +96,15 @@ export class SubjectComponent implements OnInit{
       button.setAttribute('data-target', '#addSubjectModal')
     }
     if(mode === 'edit'){
+      if(subject){
+        this.updateSubject = subject;
+      }
       button.setAttribute('data-target', '#editSubjectModal')
     }
     if(mode === 'delete'){
+      if(subject){
+        this.deleteSubject = subject;
+      }
       button.setAttribute('data-target', '#deleteSubjectModal')
     }
     container?.appendChild(button);
